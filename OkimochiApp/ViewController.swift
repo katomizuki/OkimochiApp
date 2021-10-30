7c61b6d61db635b17d116e4fc5c34e2e98954184
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-10-30 12:26:06 +0900

    Initial Commit

---
+++ OkimochiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OkimochiApp
+//
+//  Created by ミズキ on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
