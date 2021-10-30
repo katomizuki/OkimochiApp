0f39c76a1d8288921534de15db33ce99d0405854
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-10-30 13:37:21 +0900

    update UI

--- OkimochiApp/Controller/LoginController.swift
+++ OkimochiApp/Controller/LoginController.swift
@@ -3,12 +3,55 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            let leftView = UIView()
+            let imageView = UIImageView()
+            leftView.setDimensions(height: 40, width: 40)
+            emailTextField.leftView = leftView
+            emailTextField.leftViewMode = .always
+            let image = UIImage(systemName: "mail.fill")
+            imageView.image = image
+            imageView.tintColor = .systemOrange
+            leftView.addSubview(imageView)
+            imageView.setDimensions(height: 40, width: 40)
+        }
+    }
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet {
+            let imageView = UIImageView()
+            let leftView = UIView()
+            leftView.setDimensions(height: 40, width: 40)
+            passwordTextField.leftView = leftView
+            passwordTextField.leftViewMode = .always
+            let image = UIImage(systemName: "lock.fill")
+            imageView.image = image
+            imageView.tintColor = .systemOrange
+            leftView.addSubview(imageView)
+            imageView.setDimensions(height: 40, width: 40)
+
+        }
+    }
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var gotoRegisterButton: UIButton! {
+        didSet {
+            let attibutedString = NSMutableAttributedString(string: "まだアカウントを持ってない方はこちらへ\n",
+                                                     attributes: [.font : UIFont.systemFont(ofSize: 14),.foregroundColor:UIColor.systemGray])
+            attibutedString.append(NSAttributedString(string: "新規登録ページへ",
+                                                      attributes: [.font : UIFont.boldSystemFont(ofSize: 14),.foregroundColor:UIColor.systemGray]))
+            gotoRegisterButton.setAttributedTitle(attibutedString, for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
     }
 
+    @IBAction func didTapLoginButton(_ sender: Any) {
+        print(#function)
+    }
     @IBAction func gotoRegister(_ sender: Any) {
+        print(#function)
         let storyboard = UIStoryboard(name: "Auth", bundle: nil)
         print(storyboard)
         let vc = storyboard.instantiateViewController(withIdentifier: "RegisterController") as! RegisterController
--- OkimochiApp/Controller/RegisterController.swift
+++ OkimochiApp/Controller/RegisterController.swift
@@ -3,9 +3,23 @@ import UIKit
 
 class RegisterController: UIViewController {
 
+    
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var gotoRegisterButton: UIButton! {
+        didSet {
+            let attibutedText = NSMutableAttributedString(string: "既にアカウントを持っている方はこちらへ\n", attributes: [.font:UIFont.systemFont(ofSize: 14),.foregroundColor:UIColor.systemGray])
+            attibutedText.append(NSAttributedString(string: "ログイン画面へ", attributes: [.font: UIFont.boldSystemFont(ofSize: 14),.foregroundColor:UIColor.systemGray]))
+            gotoRegisterButton.setAttributedTitle(attibutedText, for: .normal)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        navigationController?.navigationBar.isHidden = true
     }
- 
+    @IBAction func gotoLogin(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
