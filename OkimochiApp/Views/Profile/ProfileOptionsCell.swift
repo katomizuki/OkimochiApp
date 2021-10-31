3456618fcbc05732faadc2d937d284513003cb60
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-10-31 17:31:38 +0900

    update cell

--- OkimochiApp/Views/Profile/ProfileOptionsCell.swift
+++ OkimochiApp/Views/Profile/ProfileOptionsCell.swift
@@ -1,9 +1,15 @@
 import UIKit
 
 class ProfileOptionsCell: UICollectionViewCell {
+    
+    @IBOutlet weak var optionsLabel: UILabel!
+    static let id = "ProfileOptionsCell"
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    static func nib()->UINib {
+        return UINib(nibName: "ProfileOptionsCell", bundle: nil)
     }
 
 }
