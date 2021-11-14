1d2a2e70f237837c9c74a598a806d3bbc785e168
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-14 20:10:42 +0900

    add googlemap search function

--- OkimochiApp/Controller/PostLetter/LetterTimeController.swift
+++ OkimochiApp/Controller/PostLetter/LetterTimeController.swift
@@ -2,10 +2,26 @@ import UIKit
 
 class LetterTimeController: UIViewController {
 
+    @IBOutlet weak var postTimePicker: UIDatePicker! {
+        didSet {
+            postTimePicker.locale = Locale(identifier: "ja_JP")
+        }
+    }
+    @IBOutlet weak var nextButton: UIButton! {
+        didSet {
+            nextButton.layer.cornerRadius = 15
+            nextButton.layer.masksToBounds = true
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.backButtonDisplayMode = .minimal
     }
   
-
+    @IBAction func didTapNextButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "PostLetter", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "LetterMapController") as! LetterMapController
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
 }
