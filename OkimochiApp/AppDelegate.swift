26fe3492a7bab1eae78b4f1fc375e163d3af1526
カトパン <76249494+katomizuki@users.noreply.github.com>
2021-11-17 19:39:28 +0900

    Update AppDelegate.swift

--- OkimochiApp/AppDelegate.swift
+++ OkimochiApp/AppDelegate.swift
@@ -10,8 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        let valid = GMSServices.provideAPIKey("AIzaSyAhr4lPJs46kNIM_UeaLuXfYnYZWmORkAo")
-        let placeValide = GMSPlacesClient.provideAPIKey("AIzaSyAhr4lPJs46kNIM_UeaLuXfYnYZWmORkAo")
+        let valid = GMSServices.provideAPIKey("API-Key")
+        let placeValide = GMSPlacesClient.provideAPIKey("API-Key")
         print(valid,"✋")
         print(placeValide,"✊")
         return true
