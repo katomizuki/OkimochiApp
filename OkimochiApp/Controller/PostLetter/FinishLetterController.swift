37fb600606dc8a50c3d13de7daacabc362fc5885
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-17 19:42:40 +0900

    bug fix

---
+++ OkimochiApp/Controller/PostLetter/FinishLetterController.swift
@@ -0,0 +1,21 @@
+//
+//  FinishLetterController.swift
+//  OkimochiApp
+//
+//  Created by ミズキ on 2021/11/17.
+//
+
+import UIKit
+
+class FinishLetterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
