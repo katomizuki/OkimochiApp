b03c16f9772a53d4cca5b60339dc3cb917a24693
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-18 12:37:26 +0900

    add Animation

--- OkimochiApp/Controller/PostLetter/FinishLetterController.swift
+++ OkimochiApp/Controller/PostLetter/FinishLetterController.swift
@@ -1,21 +1,28 @@
-//
-//  FinishLetterController.swift
-//  OkimochiApp
-//
-//  Created by ミズキ on 2021/11/17.
-//
-
 import UIKit
 
 class FinishLetterController: UIViewController {
-
+    // MARK: - Properties
+    @IBOutlet private var letterImageView: UIImageView!
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        shakeLetter()
     }
+    // MARK: -  IBAction
+    @IBAction func didTapfinishButton(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    func shakeLetter() {
+        print(#function)
+       let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 20, -20, 20, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.5
+        animation.isAdditive = true
+        letterImageView.layer.add(animation, forKey: "shake")
+    }
+
     
-
-  
-
 }
+
