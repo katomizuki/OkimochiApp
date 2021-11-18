6c77f2640c34f2465ec7fa8de55637f0ced42f2f
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-18 12:48:00 +0900

    complete vc

--- OkimochiApp/Controller/TimeLine/OpenLetterController.swift
+++ OkimochiApp/Controller/TimeLine/OpenLetterController.swift
@@ -5,8 +5,7 @@ class OpenLetterController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
     
     
