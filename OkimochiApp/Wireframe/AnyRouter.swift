36739d93ed7dd9d53e9ec215bde12eb1f08431ab
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-12-17 20:56:03 +0900

    setup Router

--- OkimochiApp/Wireframe/AnyRouter.swift
+++ OkimochiApp/Wireframe/AnyRouter.swift
@@ -6,6 +6,34 @@
 //
 
 import Foundation
-protocol AnyRouter {
-    
+import UIKit
+protocol Transitioner: AnyObject where Self:UIViewController {
+    func pushViewController(_ viewController:UIViewController,animated:Bool)
+    func popViewController(animated:Bool)
+    func popToRootViewController(_ viewController:UIViewController, animated:Bool)
+    func present(viewContoller:UIViewController,animated:Bool)
+    func dismiss(animated:Bool)
+}
+
+extension Transitioner {
+    func pushViewController(_ viewController: UIViewController,
+                            animated: Bool) {
+        guard let nc = navigationController else { return }
+        nc.pushViewController(viewController, animated: animated)
+    }
+    func popViewController(animated:Bool) {
+        guard let nc = navigationController else { return }
+        nc.popViewController(animated: animated)
+    }
+    func popToRootViewController(_ viewController:UIViewController, animated:Bool) {
+        guard let 
+    }
+    func present(viewController: UIViewController,
+                 animated: Bool,
+                 completion: (() -> ())? = nil) {
+        present(viewController, animated: animated, completion: completion)
+    }
+    func dismiss(animated: Bool) {
+        dismiss(animated: animated)
+    }
 }
